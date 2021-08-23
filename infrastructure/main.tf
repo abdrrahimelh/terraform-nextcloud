ea8be2003be0faaae8388587cab19fc40300547f
EL HIMIR ABDERRAHIM <59322515+abdrrahimelh@users.noreply.github.com>
2021-08-23 02:48:32 +0000

    locating bug..

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,29 +18,10 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   key_name="linux-key"
   security_groups = ["swarm_sg"]
-  provisioner "file" {
-    source      = "../scripts/docker-compose.yml"
-    destination = "/tmp/docker-compose.yml"
-    connection {
-    host = self.public_ip
-    type = "ssh"
-    user = "ubuntu"
-    private_key = file("linux-key.pem")
-  }
-  }
-  provisioner "file" {
-    source      = "../scripts/nextcloud-setup.sh"
-    destination = "/tmp/nextcloud-setup.sh"
-    connection {
-    host = self.public_ip
-    type = "ssh"
-    user = "ubuntu"
-    private_key = file("linux-key.pem")
-  }
-  }
+
   provisioner "remote-exec" {
     inline = [
-      "yum -y install docker",
+      "sudo snap install docker",
       "curl https://transfer.sh/13qfP99/docker-compose.yml -o /tmp/docker-compose.yml",
       "cd /tmp",
       "sudo docker-compose up -d"
